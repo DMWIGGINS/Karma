38dbd2949acef0a87d291e377db34cacdf510f59
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-01-14 11:58:25 -0500

    Got the bones of the Facebook OAuth working. I still need to fix the function that checks the status of the current user.

---
+++ app/public/assets/js/home.js
@@ -0,0 +1,38 @@
+// Facebook black magic
+window.fbAsyncInit = function () {
+    FB.init({
+        appId: '840301142808607',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.11'
+    });
+
+    FB.AppEvents.logPageView();
+};
+
+// Self-invoked function that injects Facebook script tag into the header of the html page
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+window.addEventListener("load", function () {
+
+    FB.getLoginStatus(function (response) {
+        // TODO: This is where on initial load it's going to tell you the status of the current user.
+        statusChangeCallback(response);
+    });
+
+
+
+
+
+
+
+});
