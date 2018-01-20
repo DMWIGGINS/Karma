a6f5a69f1e73989971aefe533cfa4d8199c95855
Katie DeAngelis <katie@Katies-MacBook-Pro.local>
2018-01-19 19:39:24 -0500

    Pushing user to database.

--- app/public/assets/js/home.js
+++ app/public/assets/js/home.js
@@ -29,13 +29,6 @@ window.addEventListener("load", function () {
         // statusChangeCallback(response);
     });
 
-
-
-
-
-
-
-
 });
 
 function facebookLogInCallback(response) {
@@ -43,9 +36,27 @@ function facebookLogInCallback(response) {
     // Check if the login was good
     if (response.status == "connected") {
         // Need to ask for the individual fields
-        FB.api("/me?fields=name,picture,email", function(response) {
+        FB.api("/me?fields=name,picture,email", function (response) {
             console.log(response);
             // TODO: Next need to call the server to tell it the user info
+
+            var newUser = {
+                user_name: response.name,
+                user_email: response.email,
+                fb_user_pic: response.picture.data.url,
+                fb_user_id: response.id
+            };
+
+            // Send the POST request.
+            $.ajax("/api/user/create", {
+                type: "POST",
+                data: newUser
+            }).then(
+                function () {
+                    // Reload the page to get the updated list
+                    // location.reload();
+                }
+            );
         });
     }
 }
